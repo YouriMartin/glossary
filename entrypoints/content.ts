551cb2e14d7230c6e659e9cca0859f13755f8c3d
Youri Martin <youri.martin@gmail.com>
2024-03-09 11:39:05 +0100

    update the modal position on scroll

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -26,39 +26,38 @@ export default defineContentScript({
 
         const modalText = container.querySelector("#modal-text");
 
-        document.addEventListener("mouseup", (event: MouseEvent) => {
-          new Promise<Selection>((resolve, reject) => {
-            const selected = window.getSelection();
-            if (selected !== null) {
-              resolve(selected);
-            } else {
-              reject;
-            }
-          }).then((selection) => {
-            const selectionRect = selection
-              .getRangeAt(0)
-              .getBoundingClientRect();
-            const selectedText = selection.toString().trim().toLowerCase();
+        function updateModalPosition() {
+          const selection = window.getSelection();
+          if (!selection) return;
+          const selectionRect = selection.getRangeAt(0).getBoundingClientRect();
+          const selectedText = selection.toString().trim().toLowerCase();
 
-            if (selectedText && glossary[selectedText] && modalText) {
-              modalText.textContent = `${selectedText} : ${glossary[selectedText]}`;
-              modal.style.display = "block";
-              modal.style.position = "absolute";
-              modal.style.backgroundColor = "white";
-              modal.style.padding = "1px 10px";
-              modal.style.borderRadius = "10px";
-              modal.style.color = "black";
-              modal.style.top = `${selectionRect.top - selectionRect.height}px`;
-              modal.style.left = `${selectionRect.right}px`;
-            }
-          });
-        });
+          if (selectedText && glossary[selectedText] && modalText) {
+            modalText.textContent = `${selectedText} : ${glossary[selectedText]}`;
+            modal.style.display = "block";
+            modal.style.position = "absolute";
+            modal.style.backgroundColor = "white";
+            modal.style.padding = "1px 10px";
+            modal.style.borderRadius = "10px";
+            modal.style.color = "black";
+            modal.style.top = `${
+              selectionRect.top - selectionRect.height + window.scrollY
+            }px`;
+            modal.style.left = `${selectionRect.right + window.scrollX}px`;
+          } else {
+            modal.style.display = "none";
+          }
+        }
+
+        document.addEventListener("mouseup", updateModalPosition);
 
         document.addEventListener("mousedown", (event: MouseEvent) => {
           if (!modal.contains(event.target as Node)) {
             modal.style.display = "none";
           }
         });
+
+        window.addEventListener("scroll", updateModalPosition);
       },
     });
 
