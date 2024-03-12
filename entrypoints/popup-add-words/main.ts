e1bdd10b2b2c8b87cf7b1a3ddca84ec3588ef3d1
Youri Martin <youri.martin@gmail.com>
2024-03-12 21:56:31 +0100

    init form

--- entrypoints/popup-add-words/main.ts
+++ entrypoints/popup-add-words/main.ts
@@ -3,10 +3,7 @@ import "./style.css";
 import logo from "/icon/128.png";
 import chevron from "/chevron-left.svg";
 
-export default () => {
-  console.log("addWord");
-
-  document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <div>
         <img src="${chevron}" class="logo" alt="Chevron left logo" />
@@ -14,6 +11,9 @@ export default () => {
     </div>
     <form>
         <h2>Add words</h2>
+        <label>Acronym : </label>
+        <input type="text" required\>
+        <label>Definition : </label>
+        <input type="text" required\>
     </form>
   </div>`;
-};
